917b3720f17c3e7318cf62653fc69045c39ebc0d
David <davidkramm53@gmail.com>
2022-04-05 22:01:01 -0500

    added error handaling and page redirects

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,18 +1,42 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+  };
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
             // pass response data to dom function
             displayIssues(data);
+
+            // check if api has paginated Issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
           });
         }
         else {
-          alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
       });
   };
@@ -52,5 +76,18 @@ var getRepoIssues = function(repo) {
         issueEl.appendChild(typeEl);
     };
   };
+
+  var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
   
-  getRepoIssues("DRamm1/git-it-done");
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
+  
+    getRepoName();
